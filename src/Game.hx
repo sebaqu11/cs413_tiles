a84032565dc6b4a0fbc60330ab09acd4405b44ff
Ryan Buckingham <rsb93@nau.edu>
2015-03-29 17:44:55 -0700

    Moved the pivot of the map.
    Coordinates make more sense now.

--- src/Game.hx
+++ src/Game.hx
@@ -47,8 +47,8 @@ class Game extends Sprite
 		addEventListener(Event.ENTER_FRAME, onEnterFrame);
 
 		map = new Tilemap(Root.assets, "map");
-		map.x = -16 * 22;
-		map.y = -16 * 115;
+		map.pivotX = 16 * 22;
+		map.pivotY = 16 * 115;
 		stage.addChild(map);
 		player = new Image(Root.assets.getTexture("Player"));
 		player.smoothing = "none";
